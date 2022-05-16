caf0c95cdd9de77d2caaf6476c7b0d84210a0aee
Oscar Karlsson <oscar.karl00@hotmail.com>
2022-05-16 14:56:11 +0200

    Signup screen UX

--- frontend/src/pages/authenticate/login.page.tsx
+++ frontend/src/pages/authenticate/login.page.tsx
@@ -1,15 +1,47 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import ThirdPartyLogin from "../../components/thirdPartyLogin.cmpt";
+import { useAuth } from "../../contexts/authentication.context";
+import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5";
 import "./authenticate.css"
 
 const LoginPage = () => {
+    const auth = useAuth();
+    const [hidePass, SetHidePass] = useState(true);
+    const [input, SetInput] = useState({
+        email: "",
+        password: "",
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        SetInput({
+          ...input,
+          [e.target.name]: e.target.value,
+        });
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(input)
+        auth.methods.login(
+            input.email,
+            input.password,
+            '/login'
+        );
+    }
+
     return (
         <div className="form-container">
             <div><h1>Welcome Back</h1></div>
-            <input type="email" name="email" placeholder="Email.." />
-            <input type="password" name="password" placeholder="Password.." />
-            <div>
+            <form onSubmit={handleSubmit}>
+                <div className="input-container"><input onChange={handleChange} type="email" name="email" placeholder="Email.." /></div>
+                <div className="input-container">
+                    <input onChange={handleChange} type={hidePass ? "password" : "text"} name="rePassword"autoComplete="off" placeholder="Re-Password.." />
+                    <div onClick={() => SetHidePass(!hidePass)} className="icon">{hidePass ? <IoEyeOffOutline /> : <IoEyeOutline />}</div>
+                </div>
                 <button className="btn1">Login</button>
+            </form>
+            <div>
                 <div className="hr-or">
                     <div><hr /></div>
                     <div><label>or</label></div>
@@ -19,7 +51,7 @@ const LoginPage = () => {
                     <ThirdPartyLogin />
                 </div>
                 <div className="swap-auth-container">
-                    <p>Dont't have an account?</p>
+                    <p>Don't have an account?</p>
                     <Link to={"/signup"}>Sign up</Link>
                 </div>
             </div>
--- frontend/src/pages/authenticate/signup.page.tsx
+++ frontend/src/pages/authenticate/signup.page.tsx
@@ -1,19 +1,91 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { IoEyeOutline, IoEyeOffOutline, IoCheckmarkOutline, IoClose } from "react-icons/io5";
 import ThirdPartyLogin from "../../components/thirdPartyLogin.cmpt";
+import { useAuth } from "../../contexts/authentication.context";
 import "./authenticate.css"
 
-
 const SignupPage = () => {
+    const auth = useAuth();
+    const [hidePass, SetHidePass] = useState(true);
+    const [passValidation, SetPassValidation] = useState({
+        lenght: false,
+        symbol: false,
+        number: false,
+        uppercase: false,
+        lowercase: false
+    });
+
+    const [input, SetInput] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        rePassword: "",
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+        if (e.target.name = "password") {
+
+            const symbol = new RegExp("(?=.*?[#?!@$%^&*-])");
+            const number = new RegExp("(?=.*?[0-9])");
+            const uppercase = new RegExp("(?=.*?[A-Z])");
+            const lowercase = new RegExp("(?=.*?[a-z])");
+
+            SetPassValidation({
+                lenght: e.target.value.length > 7,
+                symbol: symbol.test(e.target.value),
+                number: number.test(e.target.value),
+                uppercase: uppercase.test(e.target.value),
+                lowercase: lowercase.test(e.target.value)
+            });
+        }
+
+        SetInput({
+          ...input,
+          [e.target.name]: e.target.value,
+        });
+
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(input);
+        auth.methods.signup(
+            input.email,
+            input.firstName,
+            input.lastName,
+            input.password,
+            '/signup'
+        );
+    }
+
     return (
         <div className="form-container">
             <div><h1>Create your Account</h1></div>
-            <input type="text" name="firstName" placeholder="First name.." />
-            <input type="text" name="lastName" placeholder="Last name.." />
-            <input type="email" name="email" placeholder="Email.." />
-            <input type="password" name="password" placeholder="Password.." />
-            <input type="password" autoComplete="off" placeholder="Re-Password.." />
+            <form onSubmit={handleSubmit}>
+                <div className="input-container"><input onChange={handleChange} type="text" name="firstName" placeholder="First name.." /></div>
+                <div className="input-container"><input onChange={handleChange} type="text" name="lastName" placeholder="Last name.." /></div>
+                <div className="input-container"><input onChange={handleChange} type="email" name="email" placeholder="Email.." /></div>
+                <div className="input-container">
+                    <input onChange={handleChange} type={hidePass ? "password" : "text"} name="password" placeholder="Password.." />
+                    <div onClick={() => SetHidePass(!hidePass)} className="icon">{hidePass ? <IoEyeOffOutline /> : <IoEyeOutline />}</div>
+                </div>
+                <div className="password-hint-container">
+                    <div style={{color: passValidation.lowercase ? "black" : "#bcbecb"}}><div>{passValidation.lowercase ? <IoCheckmarkOutline /> : <IoClose />}</div><label>one uppercase</label></div>
+                    <div style={{color: passValidation.uppercase ? "black" : "#bcbecb"}}><div>{passValidation.uppercase ? <IoCheckmarkOutline /> : <IoClose />}</div><label>one lowercase</label></div>
+                    <div style={{color: passValidation.lenght ? "black" : "#bcbecb"}}><div>{passValidation.lenght ? <IoCheckmarkOutline /> : <IoClose />}</div><label>8 characters minimum</label></div>
+                    <div style={{color: passValidation.number ? "black" : "#bcbecb"}}><div>{passValidation.number ? <IoCheckmarkOutline /> : <IoClose />}</div><label>must contain one number</label></div>
+                    <div style={{color: passValidation.symbol ? "black" : "#bcbecb"}}><div>{passValidation.symbol ? <IoCheckmarkOutline /> : <IoClose />}</div><label>must contain one symbol</label></div>
+                </div>
+                <div className="input-container">
+                    <input onChange={handleChange} type={hidePass ? "password" : "text"} name="rePassword"autoComplete="off" placeholder="Confirme password.." />
+                    <div onClick={() => SetHidePass(!hidePass)} className="icon">{hidePass ? <IoEyeOffOutline /> : <IoEyeOutline />}</div>
+                </div>
+                <button className="btn1">Sign up</button>
+            </form>
             <div>
-                <button className="btn1">Sign Up</button>
                 <div className="hr-or">
                     <div><hr /></div>
                     <div><label>or</label></div>
